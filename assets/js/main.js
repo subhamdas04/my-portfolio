27d5b20952350b824a858aaa69f7e7450e005863
subhamdas04 <subhamdas0465@gmail.com>
2023-06-29 17:37:13 +0530

    potfolio

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+// ======= VISIT WEBSITE ======
+
+const visitsite = document.getElementById('visitsite');
+visitsite.addEventListener('click', function () {
+    const visitsiteURL = 'https://codekiduniya.blogspot.com/';
+    window.open(visitsiteURL, '_blank');
+});
+
+// ======= VISIT GUTHUB ======
+
+const github = document.getElementById('github');
+github.addEventListener('click', function () {
+    const visitsiteURL = 'https://github.com/subhamdas04/';
+    window.open(visitsiteURL, '_blank');
+});
+
+// ======= VISIT INSTAGRAM ======
+
+const insta = document.getElementById('insta');
+insta.addEventListener('click', function () {
+    const instaURL = 'https://www.instagram.com/_cut_appa_/';
+    window.open(instaURL, '_blank');
+});
+
+// ======= VISIT FACEBOOK ======
+
+const facebook = document.getElementById('facebook');
+facebook.addEventListener('click', function () {
+    const instaURL = 'https://www.facebook.com/subham.das.9440234/';
+    window.open(instaURL, '_blank');
+});
+
+// ======= VISIT YOUTUBE ======
+
+const uthoob = document.getElementById('uthoob');
+uthoob.addEventListener('click', function () {
+    const ytURL = 'https://www.youtube.com/channel/UCLixQrW7Z9IhfenljTKp-aQ';
+    window.open(ytURL, '_blank');
+});
+
+// ======= SEND MAIL ======
+
+const sendMailButton = document.getElementById('sendMailButton');
+sendMailButton.addEventListener('click', function () {
+    const ytURL = 'mailto:subhamdas0465@gmail.com';
+    window.open(ytURL, '_blank');
+});
+
+// ======= visit twitter ======
+
+const twitter = document.getElementById('twitter');
+twitter.addEventListener('click', function () {
+    const ytURL = 'https://twitter.com/HelloSubhammmmm';
+    window.open(ytURL, '_blank');
+});
+
+// ======= visit linkedin ======
+
+const linked = document.getElementById('linked');
+linked.addEventListener('click', function () {
+    const ytURL = 'https://www.linkedin.com/in/subham-das-087140256/';
+    window.open(ytURL, '_blank');
+});
+
+// ======= VISIT GUTHUB CSS DESIGNS ======
+
+const cssDesigns = document.getElementById('cssDesigns');
+cssDesigns.addEventListener('click', function () {
+    const visitsiteURL = 'https://github.com/subhamdas04?tab=repositories';
+    window.open(visitsiteURL, '_blank');
+});
